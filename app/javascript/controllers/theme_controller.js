452fc57217f3c79fe470463ef00874b747fbce34
Razwan Rashidi <razwan.izmi@gmail.com>
2023-02-04 17:29:06 +0800

    Add hover effect on theme toggler

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -3,21 +3,49 @@ import {Controller} from '@hotwired/stimulus'
 export default class extends Controller {
   static targets = ['toggler']
 
-  toggleTheme() {
-    if (document.documentElement.getAttribute('data-theme') === 'dark') {
+  click() {
+    if (this.currentTheme() === 'dark') {
       this.toggleLight()
     } else {
       this.toggleDark()
     }
   }
 
+  mouseover() {
+    if (this.currentTheme() === 'dark') {
+      this.renderLightIcon()
+    } else {
+      this.renderDarkIcon()
+    }
+  }
+
+  mouseout() {
+    if (this.currentTheme() === 'dark') {
+      this.renderDarkIcon()
+    } else {
+      this.renderLightIcon()
+    }
+  }
+
   toggleLight() {
     document.documentElement.setAttribute('data-theme', 'light')
-    this.togglerTarget.textContent = '🔆'
+    this.renderLightIcon()
   }
 
   toggleDark() {
     document.documentElement.setAttribute('data-theme', 'dark')
+    this.renderDarkIcon()
+  }
+
+  renderLightIcon() {
+    this.togglerTarget.textContent = '🔆'
+  }
+
+  renderDarkIcon() {
     this.togglerTarget.textContent = '🔅'
+  }
+
+  currentTheme() {
+    return document.documentElement.getAttribute('data-theme')
   }
 }
