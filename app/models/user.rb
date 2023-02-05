c9d4488b490092b756889d0236732ff61a337a6a
Razwan Rashidi <razwan.izmi@gmail.com>
2023-02-05 17:38:04 +0800

    Set up user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false }
+
+  validates :password, length: { minimum: 8 }
 end
