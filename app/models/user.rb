a862734f1deac8616e92bf0490479cd24786a5c8
Razwan Rashidi <razwan.izmi@gmail.com>
2023-02-06 11:40:01 +0800

    Add avatar to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
                        uniqueness: { case_sensitive: false }
 
   validates :password, length: { minimum: 8 }
+
+  def avatar_url
+    "https://api.dicebear.com/5.x/fun-emoji/svg?seed=#{username}"
+  end
 end
