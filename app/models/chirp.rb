2f320225ebee209e9820dc99c3550f07ac84c394
Razwan Rashidi <razwan.izmi@gmail.com>
2023-02-11 22:16:17 +0800

    Broadcast new chirp to timeline

--- app/models/chirp.rb
+++ app/models/chirp.rb
@@ -6,4 +6,12 @@ class Chirp < ApplicationRecord
   validates :content,
     presence: true,
     length: { minimum: 1, maximum: 280 }
+
+  after_create_commit(lambda do
+    broadcast_render_later_to(
+      "timeline",
+      partial: "timeline/create",
+      locals: { chirp: self },
+    )
+  end)
 end
