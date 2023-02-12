c11218d7dc4c978bc0fe60287989794808269407
Razwan Rashidi <razwan.izmi@gmail.com>
2023-02-12 14:44:55 +0800

    Specify redis free plan on render

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
+return if Chirp.all.count > 0
+
 MOVIES_GENERATOR = [
   Faker::Movies::HarryPotter,
   Faker::Movies::LordOfTheRings,
